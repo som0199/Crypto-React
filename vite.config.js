e92fa2668c0763c9284172246aff52f8db115ba7
som0199 <somyagurjar786@gmail.com>
2024-05-28 12:00:06 +0530

    api change

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server :{
-    proxy :{
-      '/api' : {
-        target : 'https://auth-backend-93eo.onrender.com',
-        changeOrigin : true,
-        secure : false,
-      }
-    }
-  }
-})
+  // server :{
+  //   proxy :{
+  //     '/api' : {
+  //       target : 'https://auth-backend-93eo.onrender.com',
+  //       changeOrigin : true,
+  //       secure : false,
+  //     }
+  //   }
+  // }
+});
